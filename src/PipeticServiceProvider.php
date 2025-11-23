87a2d5092d6f6d45a0d10a33d465d3abd44444a6
Gabriel Solomon <hello@gabrielsolomon.ro>
2025-11-23 22:52:53 +0200

    fix: translator load files

--- src/PipeticServiceProvider.php
+++ src/PipeticServiceProvider.php
@@ -14,12 +14,6 @@ class PipeticServiceProvider extends BaseBootableServiceProvider
 {
     public const NAME = 'pipetic';
 
-    public function register()
-    {
-        parent::register();
-        $this->registerResources();
-    }
-
     public function migrations(): ?string
     {
         if (PackageConfig::shouldRunMigrations()) {
@@ -29,35 +23,8 @@ class PipeticServiceProvider extends BaseBootableServiceProvider
         return null;
     }
 
-    protected function registerResources()
+    protected function translationsPath(): string
     {
-        if (false === $this->getContainer()->has('translator')) {
-            return;
-        }
-        $translator = $this->getContainer()->get('translator');
-        $folder = __DIR__ . '/Bundle/Resources/lang/';
-        $languages = $this->getContainer()->get('translation.languages');
-
-        foreach ($languages as $language) {
-            $path = $folder . $language;
-            if (is_dir($path)) {
-                $translator->addResource('php', $path, $language);
-            }
-        }
-    }
-
-    protected function registerCommands()
-    {
-//        $this->commands(
-//        );
-    }
-
-    public function provides(): array
-    {
-        return array_merge(
-            [
-            ],
-            parent::provides()
-        );
+        return __DIR__ . '/Bundle/Resources/lang/';
     }
 }
